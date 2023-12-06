88166a98117e46021070f44dddaea802a0893b58
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2023-12-06 11:39:20 -0300

    feat: input para adicionar uma tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 // import styles from './App.css'
 
 import { Header } from './components/Header'
+import { Input } from './components/Input'
+
+import styles from './App.module.css'
+
 import './global.css'
 
 function App() {
@@ -8,6 +12,10 @@ function App() {
   return (
     <div>
       <Header />
+
+      <section className={styles.content}>
+        <Input />
+      </section>
     </div>
   )
 }
